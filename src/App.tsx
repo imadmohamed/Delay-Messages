affb7899848fbf3a4700c76cc8896cacf5347260
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 00:04:08 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button>Dm Delay</Button>
+    </div>
+  )
+}
+
+export default App
