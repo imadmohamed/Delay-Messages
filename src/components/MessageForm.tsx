ae18724333147d1fc945a39e77c9c033420d3953
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 00:43:42 +0400

    using the all state value

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -6,11 +6,27 @@ import { Input } from './ui/input'
 const MessageForm = () => {
 
     const [message, setMessage] = useState<string>("");
-    const [delay, setDelay] = useState<number>(10);
+    const [delay, setDelay] = useState<number>(0);
     const [isSending, setIsSending] = useState<boolean>(false);
     const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null)
     const [sentMessage, setSentMessage] = useState<string>("");
 
+    const handleSent = () => {
+        setIsSending(true)
+
+        const id = setTimeout(() => {
+            setSentMessage(message);
+            setMessage("")
+            setIsSending(false)
+        },delay * 1000);
+        
+        setTimerId(id)
+    }
+
+    const handleCancel = () => {
+        if(timerId) clearTimeout(timerId);
+        setIsSending(false);
+    }
 
   return (
     <div className='max-w-md mx-auto mt-20 p-6 border rounded-lg shadow-sm bg-white space-y-4'>
@@ -18,16 +34,31 @@ const MessageForm = () => {
 
         <Textarea
         placeholder='Type your message...'
-        value="hello guys"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         />
 
         <Input
         type='number'
         placeholder='Delay in seconds'
-        value="5"
+        value={delay}
+        onChange={(e) => setDelay(Number(e.target.value))}
+        disabled={isSending}
         />
 
-        <Button className='w-full'>Sent with delay</Button>
+        {!isSending ? (
+        <Button className='w-full' onClick={handleSent}>Sent with delay</Button>
+
+        ) : (
+
+        <Button className='w-full' variant="destructive" onClick={handleCancel}>Cancel Sending</Button>
+        )}
+        {
+            <div className='bg-green-100 border rounded p-3 text-green-900'>
+                <p className='font font-semibold'>Message Sent:</p>
+                <p>{sentMessage}</p>
+            </div>
+        }
     </div>
   )
 }
