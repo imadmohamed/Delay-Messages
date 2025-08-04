85f654193a60ee5b4ea8b067e1b52c631d891b53
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 00:18:58 +0400

    add some component using shadcn

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MessageForm = () => {
   return (
-    <div>MessageForm</div>
+    <div className='max-w-md mx-auto mt-20 p-6 border rounded-lg shadow-sm bg-white space-y-4'>
+        <h2 className='text-2xl font-bold text-gray-800'>Dm Delay Button</h2>
+    </div>
   )
 }
 
