b846209f72d5ce31a6e3df6b295ba19abaa3a63b
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 00:28:20 +0400

    add some state valuse using react useState hook

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 
 const MessageForm = () => {
+
+    const [message, setMessage] = useState<string>("");
+    const [delay, setDelay] = useState<number>(10);
+    const [isSending, setIsSending] = useState<boolean>(false);
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null)
+    const [sentMessage, setSentMessage] = useState<string>("");
+
+
   return (
     <div className='max-w-md mx-auto mt-20 p-6 border rounded-lg shadow-sm bg-white space-y-4'>
         <h2 className='text-2xl font-bold text-gray-800'>Dm Delay Button</h2>
@@ -19,7 +27,7 @@ const MessageForm = () => {
         value="5"
         />
 
-        <Button>Sent with delay</Button>
+        <Button className='w-full'>Sent with delay</Button>
     </div>
   )
 }
